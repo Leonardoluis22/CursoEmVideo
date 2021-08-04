d5a8ebb2802bd43d20fb127e0c6f89656e5b4869
Leonardoluis22 <leonardofogaca67@gmail.com>
2021-08-04 15:04:46 -0400

    Criando scrpit

--- verificador de idade/script.js
+++ verificador de idade/script.js
@@ -3,7 +3,17 @@ function verificar(){
   let ano = data.getFullYear();
   let fano = Number(window.document.querySelector('input#txtano').value);
   let res = window.document.querySelector('div#res');
+  let idade = (ano - fano);
  
-  if (fano.value.length == 0  ) {
-      
+ 
+    if (fano.lenght.value == 0 || fano >= ano ) {
+        alert('Verifique as informações e tente novamente')
+      }else if( idade < 12 &&  ){
+        res.innerHTML=`Criança de ${idade} anos `
+
   }
+
+
+
+
+}
