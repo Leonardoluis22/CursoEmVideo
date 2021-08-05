f442be91aa7342e9d34b3b7b2432b3129c206d81
Leonardo Luís Fogaça <leonardofogaca67@gmail.com>
2021-08-05 01:36:03 -0400

    Tentando fazer funcionar...

--- verificador de idade/script.js
+++ verificador de idade/script.js
@@ -1,18 +1,52 @@
+
 function verificar(){
-  let data = new Date();
-  let ano = data.getFullYear();
-  let fano = Number(window.document.querySelector('input#txtano').value);
-  let res = window.document.querySelector('div#res');
-  let idade = (ano - fano);
- 
- 
-    if (fano.lenght.value == 0 || fano >= ano ) {
-        alert('Verifique as informações e tente novamente')
-      }else if( idade < 12 &&  ){
-        res.innerHTML=`Criança de ${idade} anos `
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = Number(document.querySelector('input#txtano').value);
+  var res = window.document.querySelector('div#res');
+  
 
+
+  if(fano == 0 || fano > ano){
+     alert('[ERRO] Verifique os dados e tente novamente!');
+  }else{
+    var fsex = document.getElementsByName('radesex');
+    var idade = (ano - fano)
+    var genero = ''
+    
+    var img = document.createElement='img'
+    img.setAttribute('id', 'foto')
+    
+    if (fsex[0].checked){
+      genero = 'Homem'
+      if(idade < 12){
+        img.createElement('src', 'crianca_masculino.png')
+      }else if(idade > 12 && idade < 18){
+        img.createElement('src', 'adolescente_Masculino.png')
+      }else if(idade > 18 && idade < 60){
+        img.createelement('src', 'home_jovem_adulto.jpg')
+      }else if(idade >= 60){
+        img.createElement('src','home_velho.png')
+      }
+
+    }else if (fsex[1].checked) {
+      genero = 'Mulher'
+      if(idade < 12){
+        img.createElement('src', 'crianca_feminina.png')
+      }else if(idade > 12 && idade < 18){
+        img.createElement('src', 'mulher_adolescente.jpg')
+      }else if(idade > 18 && idade < 60){
+        img.createelement('src', 'mulher_jovem_adulta.jpg')
+      }else if(idade >= 60){
+        img.createElement('src','mulher_velha.jpg')
+      }
+
+
+
+    }
+    res.innerHTML=`Detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
   }
-
 
 
 
