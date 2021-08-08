62d17d87686402ed537b75db03df930b8f068e3e
Leonardo Luís Fogaça <leonardofogaca67@gmail.com>
2021-08-07 21:29:19 -0400

    .

---
+++ contador/script.js
@@ -0,0 +1,18 @@
+function contar(){
+    var n1 = window.document.querySelector('input#n1');
+        n1 = Number(n1.value);
+    var n2 = window.document.querySelector('input#n2');
+        n2 = Number(n2.value);
+    var passo = window.document.querySelector('input#passo');
+        passo = Number(passo.value);
+    var res = window.document.querySelector('p#resultado');
+    
+    if( n1 == 0 || n2 == 0 || passo == 0){
+        alert('[ERRO] Corrija os valores!')
+    }else{
+        for(var c = 1; ){
+
+        }
+    }
+      
+}
